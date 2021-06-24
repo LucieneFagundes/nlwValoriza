185bc829209630294b8d121f72ffcc7279cc3d97
Luciene Nascimento <lfnascimento99@gmail.com>
2021-06-24 18:19:37 -0300

    aula3

--- src/entities/Tags.ts
+++ src/entities/Tags.ts
@@ -1,20 +1,14 @@
-import {Entity, PrimaryColumn, Column, CreateDateColumn, UpdateDateColumn} from "typeorm";
+import { Column, CreateDateColumn, Entity, PrimaryColumn, UpdateDateColumn } from "typeorm";
 import { v4 as uuid } from "uuid";
 
-@Entity("users")
-class User {
+@Entity("tags")
+class Tag{
 
     @PrimaryColumn()
-    readonly id : string;
+    readonly id: string;
 
     @Column()
     name: string;
-
-    @Column()
-    email: string;
-
-    @Column()
-    admin: boolean;
 
     @CreateDateColumn()
     created_at: Date;
@@ -29,4 +23,4 @@ class User {
     }
 }
 
-export { User };
+export {Tag}
--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,27 @@
-import express from "express";
+import express, { Request, Response, NextFunction } from "express"; import "express-async-errors";
 import { router } from "./routes";
+
 import "reflect-metadata";
 import "./database";
 
 const app = express();
 app.use(express.json());
 
-app.get("/", (request, response) => {
-    response.send("Primeira Rota");
-})
-
 app.use(router);
+
+app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+    if(err instanceof Error){
+        return response.status(400).json({
+            error: err.message
+        })
+    }
+
+    return response.status(500).json({
+        status: "error",
+        message: "Internal Server Error"
+    })
+    
+});
 
 
 //http://localhost:8090
